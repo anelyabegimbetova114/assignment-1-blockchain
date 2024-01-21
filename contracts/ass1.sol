63579e618b92141aa98c091d14ee2f7b8fb69149
anelyabegimbetova114 <124655758+anelyabegimbetova114@users.noreply.github.com>
2024-01-21 23:08:50 +0600

    Add OpenZeppelin for ERC-20 contract in the main file

--- contracts/ass1.sol
+++ contracts/ass1.sol
@@ -1,29 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
-contract AstanaITU_AnelyaToken {
-    string public name = "AstanaITU Anelya Token";
-    string public symbol = "AITUA";
-    uint256 public totalSupply = 2000;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
-    mapping(address => uint256) public balanceOf;
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
-    constructor() {
-        balanceOf[msg.sender] = totalSupply;
-        emit Transfer(address(0), msg.sender, totalSupply);
-    }
-
-    function transfer(address to, uint256 value) external returns (bool) {
-        require(to != address(0), "ERC20: Transfer to the zero address");
-        require(value <= balanceOf[msg.sender], "ERC20: Insufficient balance");
-
-        balanceOf[msg.sender] -= value;
-        balanceOf[to] += value;
-        emit Transfer(msg.sender, to, value);
-
-        return true;
+contract AstanaITU_AnelyaToken is ERC20 {
+    constructor() ERC20("AstanaITU Anelya Token", "AITUA") {
+        _mint(msg.sender, 2000 * 10**18);
     }
 
     function getLatestTransactionTimestamp() external view returns (uint256) {
@@ -36,5 +19,14 @@ contract AstanaITU_AnelyaToken {
 
     function getTransactionReceiver() external view returns (address) {
         return address(this);
+    }
+
+    function transfer(address to, uint256 value) external returns (bool) {
+        require(to != address(0), "ERC20: Transfer to the zero address");
+        require(value <= balanceOf(msg.sender), "ERC20: Insufficient balance");
+
+        _transfer(msg.sender, to, value);
+
+        return true;
     }
 }
